0c81e7d283a31f939edf3cbc1dc3e9c2b8623959
GabrielJAlvarezC <yogabe123@gmail.com>
2022-04-02 15:59:46 -0500

    testing out netlify CMS with fake blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 const { DateTime } = require("luxon")
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/css/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/admin');
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
   })
@@ -11,6 +12,6 @@ module.exports = function(eleventyConfig) {
       input: "src",
       output: "public"
     },
-    "htmlTemplateEngine": "ejs"
+    "htmlTemplateEngine": "njk"
   }
 };
